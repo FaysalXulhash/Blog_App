a97953086049dec0d9b36ed514186bd2b0d6294a
FaysalXulhash <faysalahammed80@gmail.com>
2023-07-12 22:56:08 +0600

    login and logout module added

--- users/views.py
+++ users/views.py
@@ -10,7 +10,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!!')
+            messages.success(
+                request, f'Your account has been created. You now able to log in to the system')
             return redirect('Blog-Home')
     else:
         form = UserRegisterForm()
